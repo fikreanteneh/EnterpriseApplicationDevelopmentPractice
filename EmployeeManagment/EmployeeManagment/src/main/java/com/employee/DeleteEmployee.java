4f7a45473d9d81411ef81b1e9299568cbe5a9086
fikreanteneh <fikremariam.anteneh@aait.edu.et>
2023-11-22 07:14:53 +0300

    Deletion Udated

--- EmployeeManagment/EmployeeManagment/src/main/java/com/employee/DeleteEmployee.java
+++ EmployeeManagment/EmployeeManagment/src/main/java/com/employee/DeleteEmployee.java
@@ -1,5 +1,66 @@
 package com.employee;
 
-public class DeleteEmployee {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet("/DeleteEmployee")
+public class DeleteEmployee {
+	private static final long serialVersionUID = 1L;
+    
+    public DeleteEmployee() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String query = "SELECT * FROM employee";
+		Connection connection = DBManager.getConnection();
+		PrintWriter out = response.getWriter();
+        try {
+        	Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            response.setContentType("text/html");
+            out.println("<html><body>");
+            out.println("<h2>List of Employees</h2>");
+            out.println("<table border='1'>");
+            out.println("<tr><th>ID</th><th>Name</th><th>Designation</th><th>Salary</th></tr><th>Deletion</th></tr>");
+
+            while (resultSet.next()) {
+                out.println("<tr>");
+                out.println("<td>" + resultSet.getInt("id") + "</td>");
+                out.println("<td>" + resultSet.getString("name") + "</td>");
+                out.println("<td>" + resultSet.getString("designation") + "</td>");
+                out.println("<td>" + resultSet.getDouble("salary") + "</td>");
+                out.println("<td>" + "<a href='/EmployeeManagment/DeleteEmployee?id=" + resultSet.getInt("id")  + "'>DeleteEmployee</a>" + "</td>");
+                
+                out.println("</tr>");
+            }
+
+            out.println("</table>");
+            out.println("</body></html>");
+            
+                         
+		} catch (SQLException e) {
+			e.printStackTrace();
+			 response.setContentType("text/html");
+             out.println("<html><body>");
+             out.println("<script>");
+             out.println("alert('Something Went Wrong');");
+             out.println("history.go(-1);");
+             out.println("</script>");
+             out.println("</body></html>");
+             out.close();
+		}
+	}
 }
--- EmployeeManagment/EmployeeManagment/src/main/java/com/employee/ViewEmployee.java
+++ EmployeeManagment/EmployeeManagment/src/main/java/com/employee/ViewEmployee.java
@@ -6,6 +6,11 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 
 @WebServlet("/ViewEmployee")
@@ -17,11 +22,43 @@ public class ViewEmployee extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("index.html").forward(request, response);
-	}
+		// TODO Auto-generated method stub
+		String query = "SELECT * FROM employee";
+		Connection connection = DBManager.getConnection();
+		PrintWriter out = response.getWriter();
+        try {
+        	Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            response.setContentType("text/html");
+            out.println("<html><body>");
+            out.println("<h2>List of Employees</h2>");
+            out.println("<table border='1'>");
+            out.println("<tr><th>ID</th><th>Name</th><th>Designation</th><th>Salary</th></tr>");
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+            while (resultSet.next()) {
+                out.println("<tr>");
+                out.println("<td>" + resultSet.getInt("id") + "</td>");
+                out.println("<td>" + resultSet.getString("name") + "</td>");
+                out.println("<td>" + resultSet.getString("designation") + "</td>");
+                out.println("<td>" + resultSet.getDouble("salary") + "</td>");
+                out.println("</tr>");
+            }
+
+            out.println("</table>");
+            out.println("</body></html>");
+            
+                         
+		} catch (SQLException e) {
+			e.printStackTrace();
+			 response.setContentType("text/html");
+             out.println("<html><body>");
+             out.println("<script>");
+             out.println("alert('Something Went Wrong');");
+             out.println("history.go(-1);");
+             out.println("</script>");
+             out.println("</body></html>");
+             out.close();
+		}
 	}
 
 }
